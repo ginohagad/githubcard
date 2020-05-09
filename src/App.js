5b1c73ca6e2f4ddf77f1139324a6cac298c6a440
Gino <ginohagad@gmail.com>
2020-05-09 21:40:16 +1200

    Completed basic UI and API fetch using axios.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Card, CardContent, CardMedia, Typography } from '@material-ui/core'
+import { Card, CardContent, Avatar, Typography } from '@material-ui/core'
 import axios from 'axios'
 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
 	root: {
+		display: 'flex',
+		margin: 'auto',
 		maxWidth: 345,
 	},
 	media: {
 		height: 140,
 	},
-})
+	content: {
+		margin: 'auto',
+	},
+	large: {
+		width: theme.spacing(11),
+		height: theme.spacing(11),
+		margin: 'auto',
+	},
+}))
 
 
 
@@ -19,27 +29,29 @@ const App = () => {
 	const classes = useStyles()
 
 	const [data, setData] = useState([])
-	const [isLoading, setIsLoading] = useState(true)
 
 
 	useEffect(() => {
-		async function getMyAPI() {
+		async function fetchData() {
 			const res = await axios.get(`https://api.github.com/users/ginohagad`)
 			const data = res.data
 			setData(data)
 		}
-
-		getMyAPI()
+		fetchData()
 	}, [])
 
 
 	return (
 
 		<Card className={classes.root}>
-			<CardMedia className={classes.media} image="https://placeimg.com/640/480/any" title="Test Title" />
-			<CardContent>
+			<CardContent className={classes.content}>
+				<Avatar alt="{data.name}" src={data.avatar_url} className={classes.large} />
 				<Typography gutterBottom variant="h5" component="h3">{data.login}</Typography>
-				<Typography variant="body2" color="textSecondary" component="p">This is a test typography.</Typography>
+				<Typography variant="body2" color="textSecondary" component="p">{data.location}</Typography>
+				<Typography variant="body2" color="textSecondary" component="p">Followers {data.followers}</Typography>
+				<Typography variant="body2" color="textSecondary" component="p">Repos {data.public_repos}</Typography>
+				<Typography variant="body2" color="textSecondary" component="p">Following {data.following}</Typography>
+
 			</CardContent>
 		</Card>
 	)
