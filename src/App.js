7792e46493a5fd7a57a1eff4950016c05aef0bd0
Gino <ginohagad@gmail.com>
2020-05-20 15:05:09 +1200

    Added input box - not working yet.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Card, CardContent, Avatar, Typography } from '@material-ui/core'
+import { Button, Card, CardContent, Avatar, Typography, TextField } from '@material-ui/core'
+import Icon from '@material-ui/core/Icon';
 import axios from 'axios'
 
 
 const useStyles = makeStyles((theme) => ({
-	root: {
+	container: {
+		marginTop: '20px',
+	},
+	card: {
 		display: 'flex',
 		margin: 'auto',
 		maxWidth: 345,
@@ -21,6 +25,13 @@ const useStyles = makeStyles((theme) => ({
 		height: theme.spacing(11),
 		margin: 'auto',
 	},
+	form: {
+		textAlign: 'center',
+		marginBottom: theme.spacing(3),
+	},
+	textField: {
+		marginRight: theme.spacing(2),
+	},
 }))
 
 
@@ -28,12 +39,13 @@ const useStyles = makeStyles((theme) => ({
 const App = () => {
 	const classes = useStyles()
 
+	const [user, setUser] = useState('')
 	const [data, setData] = useState([])
 
 
 	useEffect(() => {
 		async function fetchData() {
-			const res = await axios.get(`https://api.github.com/users/ginohagad`)
+			const res = await axios.get(`https://api.github.com/users/${user}/`)
 			const data = res.data
 			setData(data)
 		}
@@ -41,19 +53,28 @@ const App = () => {
 	}, [])
 
 
+	const handleChange = e => {
+		setUser(e.target.value)
+	}
+
 	return (
+		<div className={classes.container}>
+			<form className={classes.form} noValidate>
+				<TextField className={classes.textField} onChange={e => handleChange(e)} />
+	<Button variant="contained" color="primary" className={classes.button} endIcon={<Icon>send</Icon>}>Send</Button>
+			</form>
+			<Card className={classes.card}>
+				<CardContent className={classes.content}>
+					<Avatar alt="{data.name}" src={data.avatar_url} className={classes.large} />
+					<Typography gutterBottom variant="h5" component="h3">{data.login}</Typography>
+					<Typography variant="body2" color="textSecondary" component="p">{data.location}</Typography>
+					<Typography variant="body2" color="textSecondary" component="p">Followers {data.followers}</Typography>
+					<Typography variant="body2" color="textSecondary" component="p">Repos {data.public_repos}</Typography>
+					<Typography variant="body2" color="textSecondary" component="p">Following {data.following}</Typography>
+				</CardContent>
+			</Card>
+		</div>
 
-		<Card className={classes.root}>
-			<CardContent className={classes.content}>
-				<Avatar alt="{data.name}" src={data.avatar_url} className={classes.large} />
-				<Typography gutterBottom variant="h5" component="h3">{data.login}</Typography>
-				<Typography variant="body2" color="textSecondary" component="p">{data.location}</Typography>
-				<Typography variant="body2" color="textSecondary" component="p">Followers {data.followers}</Typography>
-				<Typography variant="body2" color="textSecondary" component="p">Repos {data.public_repos}</Typography>
-				<Typography variant="body2" color="textSecondary" component="p">Following {data.following}</Typography>
-
-			</CardContent>
-		</Card>
 	)
 }
 
